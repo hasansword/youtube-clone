58a560ab47781cd8f541e8f06c3f18d2ecfecfcd
Hasan Kılıç <97hasankilic@gmail.com>
2023-06-28 21:43:14 +0300

    Sidebar created and feed designed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 const Feed = () => {
   return (
-    <div>Feed</div>
-  )
-}
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+        }}>
+        SideBar
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}>
+          Copyright by Hasan Sword
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
 
-export default Feed
+export default Feed;
