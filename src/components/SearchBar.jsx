ae1e4ae5c046019b8203f5bf733586c71695eac6
Hasan Kılıç <97hasankilic@gmail.com>
2023-07-02 15:11:11 +0300

    SearchBar is used for getting dynamic results through SearchFeed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,10 +4,21 @@ import { Paper, IconButton } from "@mui/material";
 import { Search } from "@mui/icons-material";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const onHandleSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+      setSearchTerm('');
+    }
+  }
+
   return (
     <Paper
       component="form"
-      onSubmit={() => {}}
+      onSubmit={onHandleSubmit}
       sx={{
         borderRadius: 20,
         border: "1px solid #e3e3e3",
@@ -16,9 +27,9 @@ const SearchBar = () => {
         mr: { sm: 5 },
       }}>
       <input
-        onChange={() => {}}
+        onChange={(e) => setSearchTerm(e.target.value)}
         placeholder="Search..."
-        value=""
+        value={searchTerm}
         className="search-bar"
       />
       <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos } from "./";
 
+import { useParams } from "react-router-dom";
 const SearchFeed = () => {
+  const [videos, setVideos] = useState(null);
+  const {searchTerm} = useParams();
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, []);
   return (
-    <div>SearchFeed</div>
-  )
-}
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: "2" }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Results For: <span style={{ color: "#F31503" }}>{searchTerm} videos</span>
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
+};
 
-export default SearchFeed
+export default SearchFeed;
